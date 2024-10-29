9d51793a5832a25caf68910c6845399f8f97f99f
ImperatorNeron <katebarspk@gmail.com>
2024-10-29 13:34:01 +0200

    🚀 add 2 methods to sql repository

--- app/utils/sql_repository.py
+++ app/utils/sql_repository.py
@@ -31,6 +31,19 @@ class AbstractRepository(ABC):
     ) -> Optional[BaseModel]: ...
 
     @abstractmethod
+    async def fetch_by_attributes(
+        self,
+        filters: dict,
+    ) -> Optional[list[BaseModel]]: ...
+
+    @abstractmethod
+    async def fetch_one_by_attributes(
+        self,
+        name: str,
+        filters: dict,
+    ) -> Optional[BaseModel]: ...
+
+    @abstractmethod
     async def create(
         self,
         item_in: BaseModel,
@@ -66,6 +79,30 @@ class SQLAlchemyRepository(AbstractRepository):
 
         if item:
             return item.to_read_model()
+
+    async def fetch_by_attributes(
+        self,
+        **filters: dict,
+    ) -> Optional[list[BaseModel]]:
+
+        stmt = select(self.model)
+
+        for name, value in filters.items():
+            field = getattr(self.model, name, None)
+
+            if field is None:
+                # TODO: add custom exception
+                raise ValueError(f"Field '{name}' does not exist in the model.")
+
+            stmt = stmt.where(field == value)
+
+        result = await self.session.execute(stmt)
+
+        return [item.to_read_model() for item in result.scalars().all()]
+
+    async def fetch_one_by_attributes(self, **filters: dict) -> Optional[BaseModel]:
+        results = await self.fetch_by_attributes(**filters)
+        return results[0] if results else None
 
     async def create(self, item_in: BaseModel) -> BaseModel:
         stmt = insert(self.model).values(**item_in.model_dump()).returning(self.model)
